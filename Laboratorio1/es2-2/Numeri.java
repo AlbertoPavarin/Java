9a8cc9b08df0558d0207b787ef849a751389e226
AlbertoPavarin <albertopavarin@libero.it>
2023-10-24 15:34:31 +0200

    es 2.2

---
+++ Laboratorio1/es2-2/Numeri.java
@@ -0,0 +1,11 @@
+public class Numeri{
+   public static void main(String[] args)
+   {
+      System.out.println("Numero massimo rappresentabile con un double: "     +Double.MAX_VALUE);
+      System.out.println("Numero minimo rappresentabile con un double: "     +Double.MIN_VALUE);
+      System.out.println("Numero massimo rappresentabile con un float: "     +Float.MAX_VALUE);
+      System.out.println("Numero minimo rappresentabile con un float: "     +Float.MIN_VALUE);
+      System.out.println("Numero massimo rappresentabile con un integer: "     +Integer.MAX_VALUE);
+      System.out.println("Numero minimo rappresentabile con un integer: "     +Integer.MIN_VALUE);
+   }
+}
