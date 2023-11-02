67e65bcbffb239d3ea40b62a695cf3aed85b41ca
AlbertoPavarin <albertopavarin@libero.it>
2023-11-02 17:19:37 +0100

    tolti array in esercizi StandardInput

--- Laboratorio3/StandardInput2/StandardInput2.java
+++ Laboratorio3/StandardInput2/StandardInput2.java
@@ -3,10 +3,14 @@ import java.util.Scanner;
 public class StandardInput2{
     public static void main(String[] args)
     {   
+        String string, str1, str2, str3;
         Scanner input = new Scanner(System.in);
         System.out.print("Inserisci la stringa: ");
-        String string = input.nextLine();
-        String[] tokens = string.split(" ");
-        System.out.println(tokens[2] + "\n" + tokens[1] + "\n" + tokens[0]);
+        string = input.nextLine();
+        Scanner tokenizer = new Scanner(string);
+        str1 = tokenizer.next();
+        str2 = tokenizer.next();
+        str3 = tokenizer.next();
+        System.out.println(str3 + "\n" + str2 + "\n" + str1);
     }
 }
