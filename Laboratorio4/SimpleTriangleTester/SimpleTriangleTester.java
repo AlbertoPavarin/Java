e2f6f23f592c60c30634725febb8c0fd7a9ce567
AlbertoPavarin <albertopavarin@libero.it>
2023-11-07 21:46:06 +0100

    es8

--- Laboratorio4/SimpleTriangleTester/SimpleTriangleTester.java
+++ Laboratorio4/SimpleTriangleTester/SimpleTriangleTester.java
@@ -5,7 +5,7 @@ public class SimpleTriangleTester{
     {
         Scanner input = new Scanner(System.in);
 
-        int latoUno, latoDue, latoTre = 0;
+        int latoUno, latoDue, latoTre, angoloEq = 0;
         String latiInfo = "";
         String angoliInfo = "";
 
@@ -16,20 +16,29 @@ public class SimpleTriangleTester{
         System.out.print("Inserisci il lato 3 del triangolo: ");
         latoTre = input.nextInt();
 
-        if (latoUno == latoDue && latoDue == latoTre)
+        if ((latoUno < Math.abs(latoDue - latoTre) || latoDue < Math.abs(latoUno - latoTre) || latoTre < Math.abs(latoDue - latoUno)) && (latoUno > latoDue + latoTre || latoDue > latoUno + latoTre || latoTre > latoDue + latoUno))
         {
-            latiInfo = "Equilatero";
-        }
-        else if ((latoUno == latoDue && latoDue != latoTre) || (latoUno == latoTre && latoDue != latoTre))
-        {
-            latiInfo = "Isoscele";
-        }
-        else if (latoUno != latoDue && latoDue != latoTre)
-        {
-            latiInfo = "Scaleno";
+            System.out.println("Inserisci lati validi");
+            System.exit(1);
         }
 
-        System.out.println(latiInfo);
+        if (latoUno == latoDue && latoDue == latoTre)
+            latiInfo = "Equilatero";
+        else if ((latoUno == latoDue) || (latoUno == latoTre) || (latoDue == latoTre))
+            latiInfo = "Isoscele";
+        else if (latoUno != latoDue && latoDue != latoTre)
+            latiInfo = "Scaleno";
+
+        angoloEq = (latoTre * latoTre) - (latoUno * latoUno) - (latoDue * latoDue);
+
+        if (angoloEq < 0)
+            angoliInfo = "Acutangolo";
+        else if(angoloEq == 0)
+            angoliInfo = "Rettangolo";
+        else if (angoloEq > 0)
+            angoliInfo = "Ottusangolo";
+
+        System.out.println("Triangolo " + latiInfo + " " + angoliInfo);
 
         input.close();
     }
