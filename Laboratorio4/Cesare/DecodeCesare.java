6f287575398b8c80e4fb604eff3d220981526e0b
AlbertoPavarin <albertopavarin@libero.it>
2023-11-08 22:54:13 +0100

    es11 Decode

--- Laboratorio4/Cesare/DecodeCesare.java
+++ Laboratorio4/Cesare/DecodeCesare.java
@@ -4,6 +4,41 @@ public class DecodeCesare{
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        String str, encodedStr = "";
+
+        System.out.print("Inserisci una stringa crittografata: ");
+        str = input.nextLine().toUpperCase();
+
+        System.out.print("Inserisci un numero tra 1 e 25: ");
+        int n = input.nextInt();
+
         
+        if (n > 25 || n < 1)
+        {
+            System.out.println("Errore, inserire un numero tra 1 e 25");
+            System.exit(0);
+        }
+
+        for (int i = 0; i < str.length(); i++)
+        {
+            if (str.charAt(i) != ' ')
+            {
+                String encoded = Character.toString(str.charAt(i) - n);
+                if (encoded.compareTo("A") < 0)
+                {
+                    encoded = Character.toString(encoded.charAt(0) % 26 + 78);
+                }
+                
+                encodedStr += encoded;
+            }
+            else
+            {
+                encodedStr += " ";
+            }
+        }
+
+        System.out.println(encodedStr);
+
+        input.close();
     }
 }
